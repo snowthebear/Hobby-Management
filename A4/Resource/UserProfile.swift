2b5d28eb01f21fd14d04d29e5b91371f8e9dfdc3
Yenny Fransisca Halim <yhal0004@student.monash.edu>
2024-06-07 13:48:40 +1000

    implemented the searchtableviewcontroller.

---
+++ A4/Resource/UserProfile.swift
@@ -0,0 +1,8 @@
+//
+//  UserProfile.swift
+//  A4
+//
+//  Created by Yenny Fransisca Halim on 07/06/24.
+//
+
+import Foundation
---
+++ A4/Views/Cell/SearchUserCell.swift
@@ -0,0 +1,38 @@
+//
+//  SerachUserCell.swift
+//  A4
+//
+//  Created by Yenny Fransisca Halim on 07/06/24.
+//
+
+import Foundation
+import UIKit
+import SDWebImage
+import FirebaseAuth
+
+class SearchUserCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    func configure(with imageUrl: URL?, userName: String) {
+        nameLabel.text = userName
+        if let imageUrl = imageUrl {
+            profileImageView.sd_setImage(with: imageUrl, placeholderImage: UIImage(named: "defaultProfile"), options: .continueInBackground, completed: nil)
+        } else {
+            profileImageView.image = UIImage(named: "defaultProfile")
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupProfileImageView()
+    }
+
+    private func setupProfileImageView() {
+        profileImageView.layer.cornerRadius = profileImageView.frame.height / 2
+        profileImageView.clipsToBounds = true
+        profileImageView.layer.borderWidth = 1.0
+        profileImageView.layer.borderColor = UIColor.lightGray.cgColor
+    }
+}
